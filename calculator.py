50c8c47c07039a57c5faaf538b79302a6d77125a
Andres-Arvelo <67027977+Andres-Arvelo@users.noreply.github.com>
2025-11-12 23:25:13 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,5 +1,32 @@
 #Lab 3 Scientific Calculator
 import math
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero is not allowed")
+    return b / a
+def log(a, b):
+    if b <= 0:
+        raise ValueError("logarithm input must be positive")
+    if a <= 0 or a == 1:
+        raise ValueError("logarithm base must be positive and not equal to 1")
+    math.log(b, a)
+def exp(a, b):
+    return a ** b
+
+
+
+
+
+
+
+
+
 
 def print_menu(current_result):
     # Prints the "Current Result" header and the full menu, ending with the selection prompt.
